3cd5b3b298ced3d75e87775fba77627a19662a78
najmussaher <najmussaher.siddiqui@appinventiv.com>
2020-11-10 21:18:33 +0530

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+import '../styles/globals.css'
+import { Provider } from 'react-redux'
+import { useStore } from '../store'
+
+
+function MyApp({ Component, pageProps }) {
+	const store = useStore(pageProps.initialReduxState)
+	return (
+		<Provider store={store}>
+			<Component {...pageProps} />
+		</Provider>
+	)
+}
+
+export default MyApp
---
+++ pages/index.js
@@ -0,0 +1,39 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import { initializeStore } from '../store'
+
+
+// posts will be populated at build time by getStaticProps()
+export default function Home(props) {
+	return (
+		<ul>
+			{props.initialReduxState.list.map((prop) => {
+				return <li>{prop.title}</li>
+			})}
+		</ul>
+	)
+}
+
+
+export async function getServerSideProps() {
+	const reduxStore = initializeStore()
+	const { dispatch } = reduxStore
+
+	const res = await fetch('https://jsonplaceholder.typicode.com/albums')
+	const posts = await res.json()
+	dispatch({
+		type: 'UPDATE',
+		list: posts,
+		lastUpdate: Date.now(),
+	})
+	// const response = fetch('https://jsonplaceholder.typicode.com/albums').then(async res => {
+	// 	return await res.json()
+	// })
+	// dispatch({
+	// 	type: 'UPDATE',
+	// 	list: response,
+	// 	lastUpdate: Date.now(),
+	// })
+
+	return { props: { initialReduxState: reduxStore.getState() } }
+}
